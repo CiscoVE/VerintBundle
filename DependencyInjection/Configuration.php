ff14637aef3cc3205ae6c8ceaea14e1ffe7fd898
Andrew Wolder <awolder@cisco.com>
2016-09-02 13:45:16 +0200

    change config key name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $node = $treeBuilder->root('verint');
+        $node = $treeBuilder->root('verint_feedback');
         
         $node->children()
         ->scalarNode( 'wsdlurl' )->isRequired()->cannotBeEmpty()->end()
