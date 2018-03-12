807f8b0f2c7ac7f6ea0a3e35b475680ee29a8fcc
Andrew Wolder <awolder@wolder.eu>
2018-03-12 11:33:55 +0100

    Update Feedback.php

--- SoapClient/Feedback.php
+++ SoapClient/Feedback.php
@@ -520,6 +520,7 @@ class Feedback {
 		$result  = array();
 		
 		$data = $this->request('GetSurveyDataEx',$o);
+		print_r($data);
 		$xml  = XmlType::setSimpleXml($data);
 		unset($data);
 		if (!array_key_exists("error",$data))
